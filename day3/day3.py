bfb856615ac368765f1fd3d87f052ec671890c8f
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2017-12-05 15:59:57 +0100

    add day 5

--- day3/day3.py
+++ day3/day3.py
@@ -1,9 +1,18 @@
-def step_count(n):
-    pass
+def spiral_position(n):
+    edge_length = 1
+    square_side_counter = 0
+    x, y = 0, 0
+    for i in range(1, n+1):
+        # hit a corner, change direction
+        if x == y:
+            pass
+        
+        square_side_counter += 1
+
 
 
 def main():
-    pass
+    pos = spiral_position(3)
 
 
 if __name__ == '__main__':
