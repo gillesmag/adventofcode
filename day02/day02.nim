dbd2963a31a1e68f2e79ee1bad46820ed10c1f74
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2019-12-02 16:39:33 +0100

    Refactor parseProgram procedure to return seq[int] instead of seq[string]

--- day02/day02.nim
+++ day02/day02.nim
@@ -1,30 +1,30 @@
 import strutils
+import sequtils, sugar
 
-proc parseProgram(program: string): seq[string] =
-  return strutils.split(program, ",")
+proc parseProgram(program: string): seq[int] =
+  return strutils.split(program, ",").map(x => parseInt(x))
 
-proc execute(programData: seq[string], resultAt: int = 0): int =
+proc execute(programData: seq[int], resultAt: int = 0): int =
   var programData = programData
   var programCounter = 0
-  while programData[programCounter] != "99":
+  while programData[programCounter] != 99:
     var opcode = programData[programCounter]
-    var leftOperandLocation = parseInt(programData[programCounter+1])
-    var leftOperand = parseInt(programData[leftOperandLocation])
-    var rightOperandLocation = parseInt(programData[programCounter+2])
-    var rightOperand = parseInt(programData[rightOperandLocation])
-    var resultLocation = parseInt(programData[programCounter+3])
-    var result: int
+    var leftOperandPointer = programData[programCounter+1]
+    var rightOperandPointer = programData[programCounter+2]
+    var leftOperand = programData[leftOperandPointer]
+    var rightOperand = programData[rightOperandPointer]
+    var resultPointer = programData[programCounter+3]
     
-    if opcode == "1":
-      result = leftOperand + rightOperand
-    elif opcode == "2":
-      result = leftOperand * rightOperand
-    programData[resultLocation] = intToStr(result)
+    if opcode == 1:
+      programData[resultPointer] = leftOperand + rightOperand
+    elif opcode == 2:
+      programData[resultPointer] = leftOperand * rightOperand
+
     programCounter += 4
   
-  return parseInt(programData[resultAt])
+  return programData[resultAt]
 
-proc partA(programData: seq[string]): int =
+proc partA(programData: seq[int]): int =
   assert(execute(parseProgram("1,9,10,3,2,3,11,0,99,30,40,50")) == 3500)
   assert(execute(parseProgram("1,0,0,0,99")) == 2)
   assert(execute(parseProgram("2,3,0,3,99"), 3) == 6)
@@ -32,16 +32,16 @@ proc partA(programData: seq[string]): int =
   assert(execute(parseProgram("1,1,1,4,99,5,6,0,99")) == 30)
 
   var programData = programData
-  programData[1] = "12"
-  programData[2] = "2"
+  programData[1] = 12
+  programData[2] = 2
   return execute(programData)
 
-proc partB(programData: seq[string], targetValue = 19690720): int =
+proc partB(programData: seq[int], targetValue = 19690720): int =
   var programData = programData
   for noun in 0..99:
     for verb in 0..99:
-      programData[1] = intToStr(noun)
-      programData[2] = intToStr(verb)
+      programData[1] = noun
+      programData[2] = verb
       if execute(programData) == targetValue:
         return 100*noun + verb
 
