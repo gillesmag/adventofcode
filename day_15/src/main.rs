74c63946c38cf6a62ad89d2cec6ce02da19b6eb8
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2021-12-15 15:57:06 +0100

    Refactor Day 15

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,71 +1,61 @@
-use std::fs;
-use pathfinding::grid::Grid;
 use pathfinding::directed::dijkstra::dijkstra;
+use pathfinding::grid::Grid;
+use std::fs;
 
-fn main() {
-    //let filename = "test.txt";
-    let filename = "input.txt";
-
-    let file = fs::read_to_string(filename).expect("Unable to read file");
-    let weights: Vec<Vec<usize>> = file.lines().map(|line| {
-        line.chars()
-            .into_iter()
-            .filter_map(|v| String::from(v).parse().ok())
-            .collect::<Vec<usize>>()
-    }).collect();
-
+fn total_risk(weights: &Vec<Vec<usize>>) -> Option<usize> {
     let (width, height) = (weights[0].len(), weights.len());
-    //println!("{:?} {:?}", width, height);
     let mut g = Grid::new(width, height);
     g.fill();
 
-    let goal = (width-1, height-1);
-    let result = dijkstra(&(0, 0), |p| {
-        g.neighbours(*p).into_iter().map(|(x, y)| {
-            ((x, y), weights[y][x])
-        }).collect::<Vec<((usize, usize), usize)>>()
-    }, |p| *p == goal);
+    let goal = (width - 1, height - 1);
+    let result = dijkstra(
+        &(0, 0),
+        |p| {
+            g.neighbours(*p)
+                .into_iter()
+                .map(|(x, y)| ((x, y), weights[y][x]))
+                .collect::<Vec<((usize, usize), usize)>>()
+        },
+        |p| *p == goal,
+    );
 
     if let Some((_, risk)) = result {
-        println!("{}", risk);
+        return Some(risk);
     }
+    None
+}
 
-    // build new grid for second part
-    let mut g = Grid::new(width*5, height*5);
-    g.fill();
-    //println!("{:?}", g);
+fn main() {
+    let filename = "test.txt";
+    let filename = "input.txt";
 
-    let mut new_weights = vec![vec![0usize; width*5]; height*5];
-    //println!("{:?}", new_weights);
+    let file = fs::read_to_string(filename).expect("Unable to read file");
+    let weights: Vec<Vec<usize>> = file
+        .lines()
+        .map(|line| {
+            line.chars()
+                .into_iter()
+                .filter_map(|v| String::from(v).parse().ok())
+                .collect::<Vec<usize>>()
+        })
+        .collect();
 
-    let coords: Vec<(usize, usize)> = (0..height).into_iter().flat_map(|row| {
-        (0..width).into_iter().map(|col| (row, col)).collect::<Vec<(usize, usize)>>()
-    }).collect();
-    let tile_coords: Vec<(usize, usize)> = (0..5).into_iter().flat_map(|row| {
-        (0..5).into_iter().map(|col| (row, col)).collect::<Vec<(usize, usize)>>()
-    }).collect();
-    for (x,y) in coords {
-        for (tile_x, tile_y) in &tile_coords {
+    println!("{}", total_risk(&weights).unwrap());
+
+    let (width, height) = (weights[0].len(), weights.len());
+    let mut new_weights = vec![vec![0usize; width * 5]; height * 5];
+
+    let coords: Vec<(usize, usize)> = (0..height)
+        .flat_map(|y| (0..width).map(move |x| (x, y)))
+        .collect();
+    let tile_coords: Vec<(usize, usize)> =
+        (0..5).flat_map(|y| (0..5).map(move |x| (x, y))).collect();
+    for &(x, y) in coords.iter() {
+        for (tile_x, tile_y) in tile_coords.iter() {
             let new_val = (weights[y][x] + tile_x + tile_y - 1) % 9 + 1;
-            new_weights[y+tile_y*height][x+tile_x*width] = new_val;
-            //println!("{} {} {}", tile_x, tile_y, new_weights[tile_y*height][tile_x*width]);
+            new_weights[y + tile_y * height][x + tile_x * width] = new_val;
         }
     }
 
-    let goal = (width*5-1, height*5-1);
-    let result = dijkstra(&(0, 0), |p| {
-        g.neighbours(*p).into_iter().map(|(x, y)| {
-            ((x, y), new_weights[y][x])
-        }).collect::<Vec<((usize, usize), usize)>>()
-    }, |p| *p == goal);
- 
-    if let Some((_, risk)) = result {
-        println!("{}", risk);
-    }
-
-    //for (tile_x, tile_y) in &tile_coords {
-    //    println!("{} {} {}", tile_x, tile_y, new_weights[tile_y*height][tile_x*width]);
-    //}
-    //println!("{:?}", new_weights);
-    //println!("{:?}", new_weights[0]);
+    println!("{}", total_risk(&new_weights).unwrap());
 }
