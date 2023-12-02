f6e3a7eafa4e244da4bbb4885533215d9c5e165c
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2023-12-02 23:27:25 +0100

    day 1 2023

---
+++ 2023/solutions/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+INPUTS_DIR = BASE_DIR / "inputs"
+EXAMPLES_DIR = BASE_DIR / "examples"
---
+++ 2023/solutions/main.py
@@ -0,0 +1,27 @@
+from config import INPUTS_DIR, EXAMPLES_DIR
+from pathlib import Path
+import importlib
+
+def main():
+    days = sorted([p.stem for p in Path(__file__).parent.glob("day*.py")])
+
+    for didx, day in enumerate(days):
+        module = importlib.import_module(day)
+        parts = [p for p in dir(module) if p.startswith("part_")]
+        examples = [p for p in dir(module) if p.startswith("example_")]
+        message = f"Running {day}"
+        print(message)
+        print("=" * len(message))
+        for idx, example in enumerate(examples):
+            result = getattr(module, example)(open(EXAMPLES_DIR / f"{day}-{idx+1}.txt").read())
+            print(f"  Example {idx+1}:", result)
+
+        for idx, part in enumerate(parts):
+            result = getattr(module, part)(open(INPUTS_DIR / f"{day}.txt").read())
+            print(f"  Part {idx+1}   :", result)
+
+        if didx > 0:
+            print()
+
+if __name__ == "__main__":
+    main()
